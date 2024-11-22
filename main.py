65f953e45c219c8591e62027523e5aaa48df5671
mroalk <webmattr@hotmail.com>
2024-11-21 17:56:38 -0800

    Removed beautiful soup, switched to just Pandas for scraping.

--- main.py
+++ main.py
@@ -1,8 +1,14 @@
 import requests
+import pandas as pd
+import lxml
+import html5lib
 
-from bs4 import BeautifulSoup
+from json import dumps, loads
+# Switching to Pandas instead of BS4.
 
+url = 'https://en.wikipedia.org/wiki/List_of_lakes_by_depth'
+dfs = pd.read_html(url)
 
-#
-l = requests.get('https://en.wikipedia.org/wiki/List_of_lakes_by_depth')
-soup = BeautifulSoup(l.content,'html.parser')
+#Walking through the object via Python showed that dfs[1] was the desired table.
+deepest = dfs[1]
+jobj = deepest.to_json()
